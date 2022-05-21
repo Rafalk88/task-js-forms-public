043a42d18d2cd16587ed7f53bd3157e9a83383c0
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-05-21 17:21:37 +0200

    Offers added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 const uploader = document.querySelector('.uploader__input')
+const ulEl = document.querySelector('.panel__excursions')
+const liEl = document.querySelector('.excursions__item')
 
 uploader.addEventListener('change', readFile)
 
@@ -14,10 +16,24 @@ function readFile(e) {
 
             const content = readerEvent.target.result
             console.log(content.split(/[\r\n]+/gm))
+            createOffer(content)
 
         }
         reader.readAsText(file, 'UTF-8')
 
     } else { console.error('Sth not woring!') }
     
+}
+
+function createOffer(offerInfo) {
+
+    offer = offerInfo.split(/[\r\n]+/gm)
+    offer.forEach(function () {
+
+        const newLi = liEl.cloneNode(true)
+        newLi.classList.remove('excursions__item--prototype')
+        ulEl.appendChild(newLi)
+
+    })
+
 }
